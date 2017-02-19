e36adf8076fcb872613369e2b215e0f9c9cef7eb
klapantius <bazsi@klapantius.net>
2017-02-19 14:37:55 +0100

    sybi: implemented VersionInfo handling

---
+++ libs/tfsaccess/ItemSetWrapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.TeamFoundation.VersionControl.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tfsaccess
+{
+    public interface IItemSet
+    {
+        IItem[] Items { get; }
+    }
+    public class ItemSetWrapper : IItemSet
+    {
+        private ItemSet myItemSet;
+        public ItemSetWrapper(ItemSet iset)
+        {
+            myItemSet = iset;
+        }
+
+        public IItem[] Items { get { return myItemSet.Items.ToList().Select(i => new ItemWrapper(i)).ToArray(); } }
+
+    }
+}
