f6d2f7fd6b49b64840f95bd75119006a9b364380
klapantius <balazs.juhasz@klapantius.net>
2017-03-31 10:36:16 +0200

    mytools - testrunanalyzer: added threshlod and peakfilter options

--- apps/testrunanalyzer/Program.cs
+++ apps/testrunanalyzer/Program.cs
@@ -8,7 +8,7 @@ using juba.tfs.wrappers;
 using SimpleInjector;
 
 using juba.consoleapp.CmdLine;
-using Command=juba.consoleapp.CmdLine.Command;
+using Command = juba.consoleapp.CmdLine.Command;
 
 
 namespace testrunanalyzer
@@ -30,9 +30,11 @@ namespace testrunanalyzer
             var cmd = ioc.GetInstance<ICmdLineInterpreter>();
             cmd.Add(new Parameter(new[] { "build" }, "build id or build definition name (wildchars accepted)", false));
             cmd.Add(new Parameter(new[] { "days" }, "number of asked days if build definition is specified", false, "1"));
+            cmd.Add(new Parameter(new[] { "threshold", "maxmin" }, "longest accepted testrun duration in minutes", false, "20"));
+            cmd.Add(new Parameter(new[] { "peakfilter" }, "% of runs longer then threshold to take it as relevant", false, "33"));
             cmd.Add(new Parameter(new[] { "teamproject", "tp" }, "team project name", false, "syngo.net"));
             cmd.Add(new Parameter(new[] { "teamprojectcollection", "tpc" }, "team project collection uri", false, "https://tfs.healthcare.siemens.com:8090/tfs/ikm.tpc.projects"));
-            cmd.Add(new Parameter(new[] { "verbose", "v"}, "verbose mode", false, "false"));
+            cmd.Add(new Parameter(new[] { "verbose", "v" }, "verbose mode", false, "false"));
             cmd.Add(new Parameter(new[] { "anykey" }, "doesn't exit at the end", false, "false"));
             cmd.Add(new Command(new[] { "top10" }, "lists the 10 longest assemblies", () =>
             {
@@ -41,11 +43,14 @@ namespace testrunanalyzer
                     cmd.ValueOf("tp"),
                     cmd.ValueOf("build"),
                     cmd.Evaluate("days", Interpreter.DefaultIntConverter,
-                        (x) => { if (x <= 0) throw new Exception("The specified number of days is not valid"); }));
+                        (x) => { if (x <= 0) throw new Exception("The specified number of days is not valid."); }),
+                    cmd.Evaluate("threshold", Interpreter.DefaultIntConverter,
+                        (x) => { if (x <= 0) throw new Exception("The specified duration threshold is not valid."); }),
+                    cmd.Evaluate("peakfilter", Interpreter.DefaultIntConverter,
+                        (x) => { if (x <= 0 || x>100) throw new Exception("The specified peak filter is not valid. It must be in range 1-100"); })
+                    );
 
             })).Requires("build");
-            //cmd.Add(new Parameter(new[] {"Iam"}, "a name", false));
-            //cmd.Add(new Command(new []{"hello"}, "greeting", () => Out.Info("hello {0}", cmd.ValueOf("iam")))).Requires("iam");
 
             if (!cmd.Parse(args))
             {
--- apps/testrunanalyzer/TestExecutionAnalyzer.cs
+++ apps/testrunanalyzer/TestExecutionAnalyzer.cs
@@ -19,7 +19,7 @@ namespace testrunanalyzer
             myDataCollector = dataCollector;
         }
 
-        public void Analyze(string teamProjectName, string buildSpec, int days)
+        public void Analyze(string teamProjectName, string buildSpec, int days, int threshold, int peakfilter)
         {
             var spec = myBuildServer.CreateBuildDetailSpec(teamProjectName);
             spec.InformationTypes = null;
@@ -39,11 +39,13 @@ namespace testrunanalyzer
             Console.WriteLine("Top 10:");
             data.OrderByDescending(i => Convert.ToInt32((double)i.Duration.TotalMilliseconds))
                 .GroupBy(i => i.Assembly)
-                .Where(g => g.Count() > 1)
+                .Where(g => g.Average(r => r.Duration.TotalMinutes) > threshold
+                    || g.Count(r => r.Duration.Minutes > threshold) > g.Count() * peakfilter / 100)
                 .Take(10)
                 .ToList()
-                .ForEach(i =>
+                .ForEach(g =>
                 {
+                    var i = g.OrderByDescending(r => r.Duration.TotalMinutes).ToList();
                     Out.Info("\t{0}\t{1}", i.First(), myDataCollector.GetDropFolder(i.First()));
                     Out.Log("\t\t  ({0})", string.Join(", ", i.Select(r => r.Duration.ToString("g"))));
                 });
