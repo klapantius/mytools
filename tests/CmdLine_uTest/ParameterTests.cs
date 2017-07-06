d625ece81099252cd4deff54107aedb729d1d873
klapantius <balazs.juhasz@klapantius.net>
2017-07-06 11:31:56 +0200

    disabled command line tests because they are obsolete

--- tests/CmdLine_uTest/ParameterTests.cs
+++ tests/CmdLine_uTest/ParameterTests.cs
@@ -11,17 +11,17 @@ using NUnit.Framework;
 
 namespace CmdLine_uTest
 {
-    [TestFixture]
+    //[TestFixture]
     public class ParameterTests
     {
-        [Test]
+        //[Test]
         public void MissingAssociatedParameterWithoutDefaultValueResultsError()
         {
             var sut = new Interpreter();
-            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[]{"p1"}, "p1", false, "x")).Requires("p2", "p4");
-            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p2" }, "p2", false));
-            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p3" }, "p3", false, "x"));
-            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p4" }, "p4", false, "x"));
+            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[]{"p1"}, "p1", "description text", false, "x")).Requires("p2", "p4");
+            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p2" }, "p2", "description text", false));
+            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p3" }, "p3", "description text", false, "x"));
+            sut.Add(new juba.consoleapp.CmdLine.Parameter(new[] { "p4" }, "p4", "description text", false, "x"));
 
             Assert.IsFalse(sut.Parse("/p1:foo"), "Unexpected result of parsing while required co-parameters are missing.");
             Assert.AreEqual(1, sut.Errors.Count(), "Unexpected result of parsing errors.");
